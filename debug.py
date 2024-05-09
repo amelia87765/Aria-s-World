bb6fe46e648fd06deb9f376010196b8c4250b76a
amelia87765 <amelia_ziemann@wp.pl>
2024-05-09 22:05:10 +0200

    adding map file

--- debug.py
+++ debug.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame
 pygame.init()
 font = pygame.font.Font(None,30)
@@ -9,15 +8,3 @@ def debug(info,y = 10, x = 10):
 	debug_rect = debug_surf.get_rect(topleft = (x,y))
 	pygame.draw.rect(display_surface,'Black',debug_rect)
 	display_surface.blit(debug_surf,debug_rect)
-=======
-import pygame
-pygame.init()
-font = pygame.font.Font(None,30)
-
-def debug(info,y = 10, x = 10):
-	display_surface = pygame.display.get_surface()
-	debug_surf = font.render(str(info),True,'White')
-	debug_rect = debug_surf.get_rect(topleft = (x,y))
-	pygame.draw.rect(display_surface,'Black',debug_rect)
-	display_surface.blit(debug_surf,debug_rect)
->>>>>>> 048b25f0300d79159682f15716b965358e51cbc9
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import pygame, sys
 from settings import *
 
@@ -24,31 +23,4 @@ class Game:
 
 if __name__ == '__main__':
 	game = Game()
-=======
-import pygame, sys
-from settings import *
-
-class Game:
-	def __init__(self):
-
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Świat według Arii')
-		self.clock = pygame.time.Clock()
-
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-
-			self.screen.fill('black')
-			pygame.display.update()
-			self.clock.tick(FPS)
-
-if __name__ == '__main__':
-	game = Game()
->>>>>>> 048b25f0300d79159682f15716b965358e51cbc9
 	game.run()
